0a13c1481e0efcdc02885def94253c7e49021d89
Gabriel Paura <gpaura@icloud.com>
2025-02-03 18:49:21 -0300

    Initial project structure

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['m.media-amazon.com', 'i5.walmartimages.com'],
+  },
+}
+
+module.exports = nextConfig
